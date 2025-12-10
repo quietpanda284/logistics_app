22c8d5447c3ea78221a35d9b13ba07c98c229e57
Kaung San Hein <kaungsanhein727@gmail.com>
2025-12-11 00:17:38 +0630

    Updated login page bg to white

--- project-root/login.php
+++ project-root/login.php
@@ -48,7 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background-color: #121212; /* Dark Moody Background */
             color: white;
             height: 100vh;
             display: flex;
