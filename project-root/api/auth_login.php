890c11a1311e8dc13defc9ff5c405c648763f735
Kaung San Hein <kaungsanhein727@gmail.com>
2025-12-18 02:17:05 +0630

    Security update: Added prepared statement to auth_login.php

--- project-root/api/auth_login.php
+++ project-root/api/auth_login.php
@@ -4,36 +4,45 @@ include '../config/db_connect.php';
 
 header('Content-Type: application/json');
 
-// Default response
-$response = ['status' => 'error', 'message' => 'Invalid Request'];
+$response = ['status' => 'error', 'message' => 'Invalid Request.'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = '$username'";
-    $result = mysqli_query($conn, $sql);
+    $sql = "SELECT * FROM users WHERE username = ?";
 
-    if (mysqli_num_rows($result) == 1) {
-        $row = mysqli_fetch_assoc($result);
-        
-        if ($password === $row['password']) {
-            
-            $_SESSION['user_id'] = $row['user_id'];
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['role'] = $row['role'];
-    
-            echo json_encode(['status' => 'success']);
-            exit();
-    
-        } else {
-            echo json_encode(['status' => 'error', 'message' => 'Wrong Password']);
-            exit();
-        }
-    } else {
-        echo json_encode(['status' => 'error', 'message' => 'User not found']);
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo json_encode(['status' => 'error', 'message' => 'Database error.']);
         exit();
     }
+    else {
+
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt);
+
+        $result = mysqli_stmt_get_result($stmt);
+
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_assoc($result);
+            if ($password == $row['password']) {
+
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['role'] = $row['role'];
+
+                echo json_encode(['status' => 'success']);
+                exit();
+            } else {
+                echo json_encode(['status' => 'error', 'message' => 'Incorrect Password.']);
+                exit();
+            }
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'User not found.']);
+            exit();
+        }
+    }
 }
-?>
