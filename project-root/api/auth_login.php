f81b94127976444b55c81be9c60622f2617f6d67
Kaung San Hein <kaungsanhein727@gmail.com>
2025-12-19 18:09:00 +0630

    Rewrote auth_login.php myself

--- project-root/api/auth_login.php
+++ project-root/api/auth_login.php
@@ -1,48 +1,45 @@
 <?php
+
 session_start();
 include '../config/db_connect.php';
 
 header('Content-Type: application/json');
 
-$response = ['status' => 'error', 'message' => 'Invalid Request.'];
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
+    
     $sql = "SELECT * FROM users WHERE username = ?";
-
+    
     $stmt = mysqli_stmt_init($conn);
-
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        echo json_encode(['status' => 'error', 'message' => 'Database error.']);
-        exit();
-    }
-    else {
-
+    if(mysqli_stmt_prepare($stmt, $sql)) {
         mysqli_stmt_bind_param($stmt, "s", $username);
         mysqli_stmt_execute($stmt);
-
+        
         $result = mysqli_stmt_get_result($stmt);
-
-        if (mysqli_num_rows($result) == 1) {
+        
+        if(mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
-            if ($password == $row['password']) {
-
+            if($row['password'] == $_POST['password']) {
                 $_SESSION['user_id'] = $row['user_id'];
                 $_SESSION['username'] = $row['username'];
                 $_SESSION['role'] = $row['role'];
-
+                
                 echo json_encode(['status' => 'success']);
                 exit();
-            } else {
-                echo json_encode(['status' => 'error', 'message' => 'Incorrect Password.']);
+            }
+            else {
+                echo json_encode(['status' => 'error', 'message' => 'Incorrect password.']);
                 exit();
             }
-        } else {
-            echo json_encode(['status' => 'error', 'message' => 'User not found.']);
+        }
+        else {
+            echo json_encode(['status' => 'error', 'message' => 'Username is not registered.']);
             exit();
         }
     }
-}
+    else {
+        echo json_encode(['status' => 'error', 'message' => 'Database error.']);
+        exit();
+    }
+ }
